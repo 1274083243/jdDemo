34eecae2bef1ab5c7e42d6bb8bdddce3a765264b
super_ike <1274083243@qq.com>
2018-07-10 00:02:15 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("erweina").onmouseenter=function () {
+    document.getElementById("erweina_img").style.display="block";
+}
+document.getElementById("erweina").onmouseleave=function () {
+    document.getElementById("erweina_img").style.display="none";
+}
